c15acb9eca94083b0033f65876ca6fd0c67e9b59
Hal Helms <hal.helms@gmail.com>
2019-02-02 00:07:33 -0800

    added file to provide exercises in using git and GitHub

---
+++ index.js
@@ -0,0 +1,58 @@
+// Intro to Git Labs
+// 1 Feb 2019
+
+/*
+1. Clone the repo at https://github.com/WiseGuides/sample-repo-01.git
+*/
+
+/*
+2. Clone the repo at https://github.com/WiseGuides/sample-repo-02.git
+*/
+
+/*
+3. Clone the repo at https://github.com/WiseGuides/sample-repo-03.git
+*/
+
+/*
+4. Using your terminal, mkdir a new directory, temp
+*/
+
+/*
+5. cd into temp. From there mkdir of my-repo-01
+*/
+
+/*
+6. Open VS Code and use the Explorer sidebar to get to temp/my-repo--01
+*/
+
+/*
+7. Create a new file, index.html, inside temp/my-repo-01 that has a <div> element with this text in it: "This is my-repo-01"
+*/
+
+/*
+8. Go to your GitHub account on the web and create a new public repo, my-repo-01. Copy the URL for that repo
+*/
+
+/*
+9. Back in your terminal, in temp/my-repo-01, use git init to intialize your local git repo
+*/
+
+/*
+10. Add all files to the staging with git add -A
+*/
+
+/*
+11. Commit these files with a message of "initial commit" using git commit -m "initial commit"
+*/
+
+/*
+12. Set up a lync to the GitHub directory you just created by using git remote add origin <url you just copied>
+*/
+
+/*
+13. Still in terminal, sync up your local repo with the remote one on GitHub with git push -u origin master
+*/
+
+/*
+14. Verify in your GitHub account on the web that you now have an index.html file
+*/
